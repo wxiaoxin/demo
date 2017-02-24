fabc626dce007ab088e0709e0edd8ac193169887
king <wxiaoxin2b@163.com>
2017-02-24 18:51:39 +0800

    'swagger2'

---
+++ demo-web/src/main/java/com/example/demo/config/Swagger2Config.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by Administrator on 2017/2/24.
+ */
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("info.xiaomo.website"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("api根地址：http://api.xiaomo.info:8080/")
+                .termsOfServiceUrl("https://xiaomo.info/")
+                .contact(new Contact("jianxin.wang", "http://www.baidu.com", "1315474156@qq.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
--- demo-web/src/main/java/com/example/demo/web/UserController.java
+++ demo-web/src/main/java/com/example/demo/web/UserController.java
@@ -2,21 +2,29 @@ package com.example.demo.web;
 
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Administrator on 2017/2/24.
  */
 
+@Api(value = "UserController", description = "用户接口")
 @RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "查找用户", notes = "查找所有用户", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "")
+    })
     @RequestMapping("/user")
     public String all() {
         User user = new User();
@@ -24,6 +32,19 @@ public class UserController {
         user.setAge(22);
 
         userService.save(user);
+
+        return "success";
+    }
+
+    @ApiOperation(value = "查找用户", notes = "根据Id查找用户", httpMethod = "GET", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户主键id", required = true, dataType = "Long", paramType = "path")
+    })
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "用户主键id非法")
+    })
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public String get(@PathVariable String id) {
 
         return "success";
     }
